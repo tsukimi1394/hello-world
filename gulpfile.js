67887bc77315791cb59c05f8a0acc566e63c95c5
Daria Ganchukova <umi194@gmail.com>
2017-04-26 14:19:10 +0300

    transforming social icons into svg font

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,15 @@ autoprefixer = require('autoprefixer'),
 browser      = require('browser-sync'),
 sourcemaps   = require('gulp-sourcemaps'),
 pug          = require('gulp-pug'),
-gcmq          = require('gulp-group-css-media-queries');
+gcmq          = require('gulp-group-css-media-queries'),
+iconfont     = require('gulp-iconfont'),
+consolidate  = require('gulp-consolidate');
 
 gulp.task('views', function buildHTML() {
   return gulp.src('views/**/*.pug')
   .pipe(pug({
     pretty: true
-}))
+  }))
   .pipe(gulp.dest('./templates'));
 });
 
@@ -26,20 +28,40 @@ gulp.task('build:sass', function () {
  .pipe(browser.stream({match: '**/*.css'}));
 });
 
+gulp.task("build:icons", function() {
+   return gulp.src(["./assets/icons/*.svg"])//path to svg icons
+   .pipe(iconfont({
+    fontName: "svg-icons",
+     formats: ["ttf", "eot", "woff", "svg"],//зависит от браузеров, если последние браузеры то достаточно woff, woff2
+     centerHorizontally: true,
+     fixedWidth: true,
+     normalize: true
+   }))
+     .on("glyphs", function (glyphs) {     //добавление иконки   
+        gulp.src("./assets/icons/util/*.scss") // Template for scss files
+       .pipe(consolidate("lodash", { //template language
+         glyphs: glyphs,
+         fontName: "svg-icons",
+         fontPath: "../../../../../fonts/"
+       }))
+       .pipe(gulp.dest("./assets/scss/icons/"));//generated scss files with classes
+     })
+     .pipe(gulp.dest("./fonts/"));//icon font destination
+   });
 
  // Starts a BrowerSync instance
  gulp.task('serve', ['build:sass','views'], function(){
    browser.init({
-       server: {
-           baseDir: ["./", "./templates"],
-           index: "templates/ui-kite/index.html"
-       }
+     server: {
+       baseDir: ["./", "./templates", "./templates/ui-kite"],
+       index: "templates/ui-kite/index.html"
+     }
    });
-});
+ });
 
  // Runs all of the above tasks and then waits for files to change
  gulp.task('default', ['serve'], function() {
    gulp.watch(['scss/**/*.scss'], ['build:sass']);
-   gulp.watch(['views/*.pug'], ['views']);  
+   gulp.watch(['views/**/*.pug'], ['views']);  
    gulp.watch('./**/*.html').on('change', browser.reload);
-});
+ });
